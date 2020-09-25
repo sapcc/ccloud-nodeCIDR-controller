a972750c3a89269828d854fff48ebed39ac5f4a6
Marian Schwarz <marian.schwarz@sap.com>
2020-09-25 14:30:29 +0200

    first version update commented out

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/sapcc/ccloud-nodeCIDR-controller
 
+replace github.com/hosting-de-labs/go-netbox => github.com/sapcc/go-netbox v0.0.0-20200225104756-2ba979226b5a
+
 go 1.15
+
+require (
+	github.com/go-openapi/runtime v0.19.21
+	github.com/hosting-de-labs/go-netbox v1.1.6-api2.7.6
+	github.com/netbox-community/go-netbox v0.0.0-20200507032154-fbb6900a912a
+	go.uber.org/zap v1.16.0
+	k8s.io/api v0.18.6
+	k8s.io/client-go v0.18.6
+	sigs.k8s.io/controller-runtime v0.6.3
+)
---
+++ main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/hosting-de-labs/go-netbox/netbox/client"
+	"github.com/hosting-de-labs/go-netbox/netbox/client/dcim"
+	"github.com/hosting-de-labs/go-netbox/netbox/client/ipam"
+	uberzap "go.uber.org/zap"
+	corev1 "k8s.io/api/core/v1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	net2 "net"
+	url2 "net/url"
+	"os"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+func main() {
+	var debug bool
+	var kubeContext string
+	var netboxUrl string
+	var netboxToken string
+	flag.BoolVar(&debug, "debug", false, "enable debug logging")
+	flag.StringVar(&kubeContext, "kubecontext", "", "the context to use from kube_config")
+	flag.StringVar(&netboxUrl, "netboxurl", "https://netbox.global.cloud.sap", "the netbox to query for the node")
+	flag.StringVar(&netboxToken, "netboxtoken", "", "the netbox token to use")
+	flag.Parse()
+
+	ctrl.SetLogger(zap.New(func(o *zap.Options) {
+		o.Development = true
+		if ! debug {
+			o.Level = uberzap.NewAtomicLevelAt(uberzap.InfoLevel)
+		}
+	}))
+	var opts manager.Options
+	opts.MetricsBindAddress = ":8080"
+	opts.HealthProbeBindAddress = ":8081"
+
+	var log = logf.Log.WithName("ccloud-nodeCIDR-controller")
+
+	if kubeContext == "" {
+		kubeContext = os.Getenv("KUBECONTEXT")
+	}
+	restConfig, err := config.GetConfigWithContext(kubeContext)
+	if err != nil {
+		log.Error(err, "unable to setup config")
+		os.Exit(1)
+	}
+	mgr, err := manager.New(restConfig, opts)
+	if err != nil {
+		log.Error(err, "cloud not create manager")
+		os.Exit(1)
+	}
+	c, err := controller.New("ccloud-nodeCIDR-controller", mgr, controller.Options{
+		Reconciler: reconcile.Func(func(request reconcile.Request) (reconcile.Result, error) {
+			log.Info(fmt.Sprintf("Node: %s", request.Name))
+			node := &corev1.Node{}
+			mgr.GetClient().Get(context.Background(), request.NamespacedName, node)
+			if node.Spec.PodCIDR == "" {
+				log.Info("No PodCIDR set getting from netbox")
+				url, err := url2.Parse(netboxUrl)
+				if err != nil {
+					return reconcile.Result{}, err
+				}
+				transport := httptransport.New(url.Host, client.DefaultBasePath, []string{"https"})
+				transport.DefaultAuthentication = httptransport.APIKeyAuth("Authorization", "header", "Token "+netboxToken)
+				nb := client.New(transport, nil)
+				params := ipam.NewIPAMIPAddressesListParams().WithQ(&node.Name)
+				res, err := nb.IPAM.IPAMIPAddressesList(params, nil)
+				if err != nil {
+					return reconcile.Result{}, err
+				}
+				if *res.Payload.Count != 1 {
+					err := fmt.Errorf("too many results: got %d results for %s", *res.Payload.Count, node.Name)
+					log.Error(err, "error getting node ip")
+					return reconcile.Result{},err
+				}
+				deviceID := res.Payload.Results[0].Interface.Device.ID
+				var interfaceName string
+				interfaceName = "cbr0"
+				log.Info(fmt.Sprintf("looking for device %d and interface %s", deviceID, interfaceName))
+				deviceParams := dcim.NewDcimInterfacesListParams().WithDeviceID(&deviceID).WithName(&interfaceName)
+				cbr0, err := nb.Dcim.DcimInterfacesList(deviceParams, nil)
+				if err != nil {
+					return reconcile.Result{}, err
+				}
+				if *cbr0.Payload.Count != 1 {
+					err := fmt.Errorf("too many results: got %d results for device %d", *cbr0.Payload.Count, deviceID)
+					log.Error(err, "error getting node device")
+					return reconcile.Result{},err
+				}
+				ipParams := ipam.NewIPAMIPAddressesListParams().WithInterfaceID(&cbr0.Payload.Results[0].ID)
+				theIP, err := nb.IPAM.IPAMIPAddressesList(ipParams, nil)
+				if err != nil {
+					return reconcile.Result{}, err
+				}
+				if *theIP.Payload.Count != 1 {
+					err := fmt.Errorf("too many results: got %d results for interface %d", *theIP.Payload.Count, cbr0.Payload.Results[0].ID)
+					log.Error(err, "error getting node device")
+					return reconcile.Result{},err
+				}
+				_, net, err := net2.ParseCIDR(*theIP.Payload.Results[0].Address)
+				log.Info(fmt.Sprintf("net: %s", net.String()))
+				node.Spec.PodCIDR = net.String()
+				//mgr.GetClient().Update(context.Background(), node)
+			}
+			return reconcile.Result{}, nil
+		}),
+	})
+	if err != nil {
+		log.Error(err, "unable to create ccloud-nodeCIDR-controller")
+		os.Exit(1)
+	}
+	err = c.Watch(&source.Kind{Type: &corev1.Node{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		log.Error(err, "unable to watch nodes")
+		os.Exit(1)
+	}
+	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
+		log.Error(err, "unable to continue running manager")
+		os.Exit(1)
+	}
+}
