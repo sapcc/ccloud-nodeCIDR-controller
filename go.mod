549366333c801d084b9b2e2712981c94ad40cf02
Marian Schwarz <marian.schwarz@sap.com>
2020-11-03 10:52:13 +0100

    move to go-netbox-go

--- go.mod
+++ go.mod
@@ -5,11 +5,14 @@ replace github.com/hosting-de-labs/go-netbox => github.com/sapcc/go-netbox v0.0.
 go 1.15
 
 require (
-	github.com/go-openapi/runtime v0.19.21
-	github.com/hosting-de-labs/go-netbox v1.1.6-api2.7.6
-	github.com/netbox-community/go-netbox v0.0.0-20200507032154-fbb6900a912a
+	github.com/kr/text v0.2.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/prometheus/client_golang v1.0.0
+	github.com/sapcc/go-netbox-go v0.0.0-20200916104853-2806c4993772
 	go.uber.org/zap v1.16.0
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776 // indirect
 	k8s.io/api v0.18.6
 	k8s.io/client-go v0.18.6
 	sigs.k8s.io/controller-runtime v0.6.3
--- main.go
+++ main.go
@@ -4,16 +4,14 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	httptransport "github.com/go-openapi/runtime/client"
-	"github.com/hosting-de-labs/go-netbox/netbox/client"
-	"github.com/hosting-de-labs/go-netbox/netbox/client/dcim"
-	"github.com/hosting-de-labs/go-netbox/netbox/client/ipam"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sapcc/go-netbox-go/dcim"
+	"github.com/sapcc/go-netbox-go/ipam"
+	"github.com/sapcc/go-netbox-go/models"
 	uberzap "go.uber.org/zap"
 	corev1 "k8s.io/api/core/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 	net2 "net"
-	url2 "net/url"
 	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
@@ -96,57 +94,70 @@ func main() {
 			mgr.GetClient().Get(context.Background(), request.NamespacedName, node)
 			if node.Spec.PodCIDR == "" {
 				log.Info("No PodCIDR set getting from netbox")
-				url, err := url2.Parse(netboxUrl)
+				nbIpam, err := ipam.New(netboxUrl, netboxToken)
 				if err != nil {
 					return reconcile.Result{}, err
 				}
-				transport := httptransport.New(url.Host, client.DefaultBasePath, []string{"https"})
-				transport.DefaultAuthentication = httptransport.APIKeyAuth("Authorization", "header", "Token "+netboxToken)
-				nb := client.New(transport, nil)
-				params := ipam.NewIPAMIPAddressesListParams().WithQ(&node.Name)
-				res, err := nb.IPAM.IPAMIPAddressesList(params, nil)
+				opts := models.ListIpAddressesRequest{}
+				opts.Q = node.Name
+				res, err := nbIpam.ListIpAddresses(opts)
 				if err != nil {
 					log.Error(err, "error searching ips for hostname")
 					netboxFails.Inc()
 					return reconcile.Result{}, err
 				}
-				if *res.Payload.Count != 1 {
-					err := fmt.Errorf("too many results: got %d results for %s", *res.Payload.Count, node.Name)
+				if res.Count != 1 {
+					err := fmt.Errorf("too many results: got %d results for %s", res.Count, node.Name)
 					log.Error(err, "error getting node ip")
 					netboxResultFails.Inc()
 					return reconcile.Result{},err
 				}
-				deviceID := res.Payload.Results[0].Interface.Device.ID
+				log.Info(fmt.Sprintf("%+v", res.Results[0]))
+				deviceID := res.Results[0].AssignedInterface.Device.Id
 				var interfaceName string
 				interfaceName = "cbr0"
 				log.Info(fmt.Sprintf("looking for device %d and interface %s", deviceID, interfaceName))
-				deviceParams := dcim.NewDcimInterfacesListParams().WithDeviceID(&deviceID).WithName(&interfaceName)
-				cbr0, err := nb.Dcim.DcimInterfacesList(deviceParams, nil)
+				//deviceParams := dcim.NewDcimInterfacesListParams().WithDeviceID(&deviceID).WithName(&interfaceName)
+				interfaceOpts := models.ListInterfacesRequest{
+					DeviceId: deviceID,
+				}
+				interfaceOpts.Name = interfaceName
+				//cbr0, err := nb.Dcim.DcimInterfacesList(deviceParams, nil)
+				nbDcim, err := dcim.New(netboxUrl, netboxToken)
+				if err != nil {
+					log.Error(err, "error getting dcim client")
+					netboxFails.Inc()
+					return reconcile.Result{}, err
+				}
+				interf, err := nbDcim.ListInterfaces(interfaceOpts)
 				if err != nil {
 					log.Error(err, "error searching interfaces")
 					netboxFails.Inc()
 					return reconcile.Result{}, err
 				}
-				if *cbr0.Payload.Count != 1 {
-					err := fmt.Errorf("too many results: got %d results for device %d", *cbr0.Payload.Count, deviceID)
+				if interf.Count != 1 {
+					err := fmt.Errorf("too many results: got %d results for device %d", interf.Count, deviceID)
 					log.Error(err, "error getting node device")
 					netboxResultFails.Inc()
 					return reconcile.Result{},err
 				}
-				ipParams := ipam.NewIPAMIPAddressesListParams().WithInterfaceID(&cbr0.Payload.Results[0].ID)
-				theIP, err := nb.IPAM.IPAMIPAddressesList(ipParams, nil)
+				//ipParams := ipam.NewIPAMIPAddressesListParams().WithInterfaceID(&cbr0.Payload.Results[0].ID)
+				ipOpts := models.ListIpAddressesRequest{}
+				ipOpts.InterfaceId = interf.Results[0].Id
+				//theIP, err := nb.IPAM.IPAMIPAddressesList(ipParams, nil)
+				theIP, err := nbIpam.ListIpAddresses(ipOpts)
 				if err != nil {
 					log.Error(err, "error searching cbr0 ip")
 					netboxFails.Inc()
 					return reconcile.Result{}, err
 				}
-				if *theIP.Payload.Count != 1 {
-					err := fmt.Errorf("too many results: got %d results for interface %d", *theIP.Payload.Count, cbr0.Payload.Results[0].ID)
+				if theIP.Count != 1 {
+					err := fmt.Errorf("too many results: got %d results for interface %d", theIP.Count, interf.Results[0].Id)
 					log.Error(err, "error getting node device")
 					netboxResultFails.Inc()
 					return reconcile.Result{},err
 				}
-				_, net, err := net2.ParseCIDR(*theIP.Payload.Results[0].Address)
+				_, net, err := net2.ParseCIDR(theIP.Results[0].Address)
 				log.Info(fmt.Sprintf("net: %s", net.String()))
 				node.Spec.PodCIDR = net.String()
 				err = mgr.GetClient().Update(context.Background(), node)
